d82d47e0521b3a344ed8e01d29f79ac859f1ffd8
amey09 <ameymhatre09@gmail.com>
2023-10-31 22:19:21 +0530

    bug: reproduce theme conflict
    This commit reproduces following error message caused when MobileDatePicker from material ui is used along with chakra ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, ChakraProvider, Flex, Heading, Text } from "@chakra-ui/react";
+import { LocalizationProvider, MobileTimePicker } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ChakraProvider>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Flex
+          height={"100svh"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          flexDir={"column"}
+          gap={"1.5rem"}
+          bgColor={"green.100"}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Heading>Chakra UI / Material UI</Heading>
+          <Text color={"red.300"}>Theme Conflict issue !</Text>
+          <Flex flexDir={"column"}>
+            <Button colorScheme="yellow">Open Material UI's DatePicker</Button>
+            <Flex>
+              <MobileTimePicker defaultValue={dayjs("2022-04-17T15:30")} />
+            </Flex>
+          </Flex>
+        </Flex>
+      </LocalizationProvider>
+    </ChakraProvider>
   );
 }
 
