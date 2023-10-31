4ff6ee1fc14a1623a986798cf40ca17484a051b5
Amey Mhatre <75194382+amey09@users.noreply.github.com>
2023-10-31 17:10:23 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -6,25 +6,31 @@ import dayjs from "dayjs";
 function App() {
   return (
     <ChakraProvider>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <Flex
-          height={"100svh"}
-          justifyContent={"center"}
-          alignItems={"center"}
-          flexDir={"column"}
-          gap={"1.5rem"}
-          bgColor={"green.100"}
-        >
-          <Heading>Chakra UI / Material UI</Heading>
-          <Text color={"red.300"}>Theme Conflict issue !</Text>
-          <Flex flexDir={"column"}>
-            <Button colorScheme="yellow">Open Material UI's DatePicker</Button>
-            <Flex>
-              <MobileTimePicker defaultValue={dayjs("2022-04-17T15:30")} />
-            </Flex>
+      {/* <LocalizationProvider dateAdapter={AdapterDayjs}> */}
+      <Flex
+        height={"100svh"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        flexDir={"column"}
+        gap={"1.5rem"}
+        bgColor={"green.100"}
+      >
+        <Heading>Chakra UI / Material UI</Heading>
+        <Text color={"red.300"}>Theme Conflict issue !</Text>
+        <Text>
+          Uncomment LocalizationProvider and Mobile TimePicker to trigger the
+          issue
+        </Text>
+        <Flex flexDir={"column"}>
+          <Button colorScheme="yellow">
+            Try using Material UI's DatePicker
+          </Button>
+          <Flex>
+            {/* <MobileTimePicker defaultValue={dayjs("2022-04-17T15:30")} /> */}
           </Flex>
         </Flex>
-      </LocalizationProvider>
+      </Flex>
+      {/* </LocalizationProvider> */}
     </ChakraProvider>
   );
 }
